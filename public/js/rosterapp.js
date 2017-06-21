a49bd2f5f62bdf366d2de6116c7f1fd1be8bbb0a
Cody <cody@hastings.email>
2017-06-21 16:20:50 -0500

    adding exercises

---
+++ public/js/rosterapp.js
@@ -0,0 +1,21 @@
+	
+    // **************Bonus**************
+
+        // ***********  Array Manipulation Optional BONUS Exercise...
+        // ***********  Create a student roster management app. Using the user dialogue functions (alert, confirm, prompt), create an application that will prompt the user to choose from a menu with the following actions: 
+        // ***********  1) Add a Student 
+        // ***********  2) Delete a Student 
+        // ***********  3) view all students in alphabetical order 
+        // ***********  4) view all students in reverse alphabetical
+(function(){
+	"use strict"
+	
+
+
+
+
+
+
+
+
+});
