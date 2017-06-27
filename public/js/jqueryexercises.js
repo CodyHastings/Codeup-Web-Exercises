76f6e3234ee22b7e77bd5f103a4e805766e0b72f
Cody <cody@hastings.email>
2017-06-27 12:22:36 -0500

    adding JS quiz, and Jquery excersises

---
+++ public/js/jqueryexercises.js
@@ -0,0 +1,7 @@
+var contents = $('#mainH1').html();
+alert(contents);
+
+// $('.fancy').css('border', '1px solid red')
+
+$('li').css('font-size', '20px')
+$('h1,p,li').css('background-color', '#FF0');
