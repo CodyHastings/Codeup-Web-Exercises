b6a34eee02d3201df42e74a007d188cec68d5aab
Cody <cody@hastings.email>
2017-07-31 16:59:05 -0500

    setting up counter exercise

---
+++ public/counter.php
@@ -0,0 +1,5 @@
+<!-- In your HTML, you will need two links. One that says up and another that says down.
+Add a heading that shows a number representing the current counter value. This value will start at zero. When up is clicked, the counter value should increase; when down is clicked, it should decrease.
+The up and down links will send GET requests back to the counter page itself.
+Create a function that will access the $_GET superglobal variable. It should determine what the new counter value is and return it.
+Use the extract() function to change the return value of the pageController() into variables for your HTML. -->
