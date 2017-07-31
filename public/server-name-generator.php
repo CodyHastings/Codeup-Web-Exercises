a3b3b5c7e804ccaae2f05e318ebb4ff289517d3f
Cody <cody@hastings.email>
2017-07-31 11:22:39 -0500

    adding completed name gernator exercise

---
+++ public/server-name-generator.php
@@ -0,0 +1,43 @@
+<?php
+
+// Create two arrays.
+// One containing at least 10 adjectives.
+// Another containing at least 10 nouns.
+// Create a function that will return a random element from an array.
+// Create a function that returns the string value of our new server name made by 
+// combining a random adjective with a random noun.
+// Each time you refresh the page a different result should be displayed.
+
+$adjarr = ["Sassy", "Tremendous", "Old-Fashioned", "Mere", "Substantial", "Outrageous", "Ruthless", "Puzzled", "Redundant", "Jazzy", "Atomic", "Quaint", "Wooden", "Shiny", "Ruddy"];
+
+$nounarr = ["Coffee", "Covfefe","Hand-Shake", "HI-5","Fish", "Jams","Lyrics", "Battle","Doug", "Emmett","Avery", "Narwhals", "Yo-yo", "Elbows", "Knee-caps"];
+
+function randomElm($adj, $noun)
+{
+	$adjindex = array_rand($adj);
+	$nounindex = array_rand($noun);
+
+	return $adj[$adjindex] . " " . $noun[$nounindex];
+
+}
+
+// function serverName()
+// {
+
+// }
+
+$serverName = randomElm($adjarr, $nounarr);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>server name generator</title>
+</head>
+<body>
+
+	<h1><?php echo $serverName ?> </h1>
+
+</body>
+</html>
