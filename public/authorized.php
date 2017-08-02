8249c7c3ed7cdac0371cc47bb97847e908aa8bcb
Cody <cody@hastings.email>
2017-08-02 16:10:41 -0500

    adding completed POST exercise

--- public/authorized.php
+++ public/authorized.php
@@ -1,5 +1,44 @@
 <?PHP
+	session_start();
 
+function pageController() 
+{
+
+
+	var_dump($_SESSION);
+
+	if (!isset($_SESSION['logged_in_user'])){
+		header("Location:login.php");
+		die();
+	} 
+
+	if (isset($_POST['logout'])) {
+		logOut();
+		var_dump($_POST['logout']);
+		header("Location:login.php");
+		die();
+	}
+
+}
+
+function logOut()
+{
+    // clear $_SESSION array
+    session_unset();
+
+    // delete session data on the server
+    session_destroy();
+
+    // ensure client is sent a new session cookie
+    session_regenerate_id();
+
+    // start a new session - session_destroy() ended our previous session so
+    // if we want to store any new data in $_SESSION we must start a new one
+    session_start();
+
+}
+
+pageController();
 
 ?>
 
@@ -10,6 +49,13 @@
 	<title>Authorized</title>
 </head>
 <body>
+
+<h1>Thank you for logging in <?= $_SESSION['logged_in_user']?></h1>
+
+<form method="POST">
+	<button name="logout" type="submit">Log out</button>
+</form>
+
 
 </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -1,14 +1,64 @@
-Create a file named login.php in your codeup.dev site. 
-Add a form that accepts a username and password and have the form submit to the same page (login.php).
-Create a file named authorized.php in your codeup.dev site. 
-Add some HTML that simply says "Authorized" when it is viewed in the browser.
-Add some PHP code to the top of the login page that checks the POST'ed username and password. 
-If the username is equal to "guest" and the password is equal to "password" then redirect to the authorized.php. 
-If the username and password do not match those values, show a login failed message on the login page.
+<!-- Open the login.php page. If the correct username and password are sent, 
+assign a session key called logged_in_user to the username of the logged in user.
+
+
+
+Add a check to the login.php page to see if the user is logged in by checking the logged_in_user key. 
+If they are, redirect to the authorized.php page instead of showing the login page.
+Add a check to the authorized.php page to redirect to the login.php page if a user is not logged in. 
+If they are, display their username after the authorized message.
+Add a logout.php file. Use the clearSession() function from the examples in this lesson to perform a 
+logout when that page is accessed and then redirect the user to the login page.
+Add a link to the authorized.php page that goes to logout.php.
+Test logging in, logging out, and accessing the authorized page without being logged in to make sure 
+everything works as expected. -->
 
 
 <?php
+	var_dump($_POST);
 
+function pageController() 
+{
+
+	session_start();
+	if (isset($_SESSION['logged_in_user'])){
+		header("Location:authorized.php");
+	}
+	$data['error'] = null;
+
+
+
+	if (isset($_POST['username']) && isset($_POST['password'])){
+
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+
+
+	} else {
+		$username = '';
+		$password = '';
+	}
+
+	if ($username == 'guest' && $password == 'password') {
+		
+		$sessionId = session_id();
+		$_SESSION['logged_in_user'] = $username;
+		$_SESSION['sessionID'] = $sessionId;
+		var_dump($sessionId);
+		header("Location:authorized.php");
+		die();
+	} elseif ($username !== '' && $password !== '') {
+		$data['error'] = "Invalid username or password try again.";
+	}
+
+
+
+
+	return $data;
+
+}
+
+extract(pageController());
 
 ?>
 
@@ -22,7 +72,15 @@ If the username and password do not match those values, show a login failed mess
 </head>
 <body>
 
+	<h1><?=$error?></h1>
 
+    <form method="POST" action="login.php">
+        <label>Username</label>
+        <input type="text" name="username"><br>
+        <label>Password</label>
+        <input type="text" name="password"><br>
+        <input type="submit">
+    </form>
 
 
 </body>
