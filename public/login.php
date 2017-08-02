fa6c2bfdf3c21fa7767fafc73fe93b87366e5b6c
Cody <cody@hastings.email>
2017-08-02 11:07:27 -0500

    setting up login exercize

---
+++ public/login.php
@@ -0,0 +1,29 @@
+Create a file named login.php in your codeup.dev site. 
+Add a form that accepts a username and password and have the form submit to the same page (login.php).
+Create a file named authorized.php in your codeup.dev site. 
+Add some HTML that simply says "Authorized" when it is viewed in the browser.
+Add some PHP code to the top of the login page that checks the POST'ed username and password. 
+If the username is equal to "guest" and the password is equal to "password" then redirect to the authorized.php. 
+If the username and password do not match those values, show a login failed message on the login page.
+
+
+<?php
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login!!!!</title>
+</head>
+<body>
+
+
+
+
+</body>
+</html>
