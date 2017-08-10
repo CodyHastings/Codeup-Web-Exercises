b5dc12d54adf5281e4da2f226157a9e9924edd26
Cody <cody@hastings.email>
2017-08-10 16:09:27 -0500

    working migration and seeder files

---
+++ parks_migration.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "park_logins.php";
+require_once "db_connect.php";
+
+
+
+$message = 'drop table if exists national_parks;
+create table if not exists national_parks(
+  id int unsigned not null auto_increment,
+  name varchar(150) not null,
+  location varchar(300) not null,
+  date_established date not null,
+  area_in_acres double(12,2) not null,
+  primary key (id)
+);';
+
+$dbc->exec($message);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
