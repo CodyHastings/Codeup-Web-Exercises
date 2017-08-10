9843ed5f514d669a1c77ddb3e0037acc78a61fc1
Cody <cody@hastings.email>
2017-08-10 13:53:30 -0500

    added db_connect and env in git ignore

---
+++ db_connect.php
@@ -0,0 +1,9 @@
+<?php 
+
+require_once "env.php";
+
+$dbc = new PDO("mysql:host=127.0.0.1;dbname=$dbName", $username, $password);
+
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
