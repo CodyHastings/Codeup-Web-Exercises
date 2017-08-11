1351058547d843429b18a769484e52ef11c87321
Cody <cody@hastings.email>
2017-08-11 11:10:55 -0500

    set up page controller and incrementation/decrementation of page number through get request

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php 
-
+require_once "park_logins.php";
 
 
 $dbc = new PDO("mysql:host=" . DB_HOSTNAME . ";dbname=" . DB_DBNAME, DB_USERNAME, DB_PASSWORD);
--- public/national_parks.php
+++ public/national_parks.php
@@ -1,5 +1,33 @@
 <?php
  require_once __DIR__ . '/../db_connect.php';
+require_once '../Input.php';
+
+function pageController()
+{
+
+if(!empty($_REQUEST)){
+	$number = Input::get('pageNum');
+
+
+
+	$pageNum = ['pageNum' => $number];
+ 
+}else{
+
+	$pageNum = ['pageNum' => 0];
+	
+}
+
+
+
+
+return $pageNum;
+
+}
+
+extract(pageController());
+
+
 
 ?>
 
@@ -9,6 +37,13 @@
 	<title>National Parks!</title>
 </head>
 <body>
+
+	<h1> National Parks!!!</h1>
+
+	<a href="national_parks.php?pageNum=<?=$pageNum-1?>">Prev </a>
+	<a href="national_parks.php?pageNum=<?=$pageNum+1?>">Next </a>
+
+	<h1>Page <?= $pageNum ?> </h1>
 
 </body>
 </html>
