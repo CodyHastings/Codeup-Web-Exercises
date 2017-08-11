6b8389036709892241d18696db3957df1bed002e
Cody <cody@hastings.email>
2017-08-11 13:48:40 -0500

    completed functionality of getting table data to display

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,31 +1,34 @@
 <?php
- require_once __DIR__ . '/../db_connect.php';
+require_once __DIR__ . '/../db_connect.php';
 require_once '../Input.php';
 
-function pageController()
+function pageController($dbc)
 {
 
-if(!empty($_REQUEST)){
+if (!empty($_REQUEST)) {
 	$number = Input::get('pageNum');
+	$offset = $number * 4;
 
+	$stmt = $dbc->query("SELECT * FROM national_parks limit 4 offset $offset");
+	$stmt->fetch(PDO::FETCH_ASSOC);
 
-
-	$pageNum = ['pageNum' => $number];
+	$data = ['pageNum' => $number, 'stmt' => $stmt];
  
 }else{
+	$stmt = $dbc->query("SELECT * FROM national_parks limit 4 ");
+	$stmt->fetch(PDO::FETCH_ASSOC);
 
-	$pageNum = ['pageNum' => 0];
+	$data = ['pageNum' => 1, 'stmt' => $stmt];
 	
 }
 
 
-
-
-return $pageNum;
+	return $data;
 
 }
 
-extract(pageController());
+
+extract(pageController($dbc));
 
 
 
@@ -40,8 +43,26 @@ extract(pageController());
 
 	<h1> National Parks!!!</h1>
 
-	<a href="national_parks.php?pageNum=<?=$pageNum-1?>">Prev </a>
-	<a href="national_parks.php?pageNum=<?=$pageNum+1?>">Next </a>
+<table>
+	<tr>
+	<th>Park Name</th>
+	<th>Location</th>
+	<th>Date Established</th>
+	<th>Area in acres</th>
+</tr>
+
+	<?PHP foreach ($stmt as $value) : ?>
+<tr>
+	<td><?= $value['name'] ?></td>
+	<td><?= $value['location'] ?></td>
+	<td><?= $value['date_established'] ?></td>
+	<td><?= $value['area_in_acres'] ?></td>
+</tr>
+	<?PHP endforeach ?>
+</table>
+
+	<a href="national_parks.php?pageNum=<?=$pageNum-1?>&c=prev">Prev </a>
+	<a href="national_parks.php?pageNum=<?=$pageNum+1?>&c=next">Next </a>
 
 	<h1>Page <?= $pageNum ?> </h1>
 
